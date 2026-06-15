51927c50546b28baddefea782be9eb00e3f6dc83
TinTun123 <58549019+TinTun123@users.noreply.github.com>
2026-06-15 13:51:52 +0700

    Edit home title

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -10,7 +10,7 @@
 
         <!-- Optional Footer -->
         <footer class="bg-[#FFFDF6] text-center text-[10px] text-gray-600">
-            © 2025 Overseas Association Association
+            © 2025 Overseas Irrawaddy Association
         </footer>
     </div>
 </template>
